00e26665932c067822fab3d154abba06d908fb59
Ver0-PC <jakub.wrobel27@gmail.com>
2019-08-19 13:49:02 +0200

    Added first upgrade and polished upgrade area

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function klik() {
   document.getElementById("count").innerHTML = cookies;
   console.log("Cookies:"+cookies);
 }
+
+function upg1() {
+  if (cookies >= 10) {
+    cursor = cursor + 1;
+    console.log("Cursor"+cursor);
+    cookies = cookies - 10;
+    document.getElementById("count").innerHTML = cookies;
+  } else {
+    console.log("Not enough cookies");
+  }
+
+}
